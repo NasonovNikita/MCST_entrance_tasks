73814357c96858b10e189dd1c313cc094bc088da
Nasonov Nikita <Cyclist2006@yandex.ru>
2026-03-11 11:35:54 +0300

    Allocation fix

--- task1/main.c
+++ task1/main.c
@@ -20,17 +20,39 @@ void* thread_sort(void* data);
 void launch_thread_sorts(int *arr, int size, int num_threads);
 
 // Saves array from stdin to arr, returns size of array
-int scan_arr(int* arr) {
-    int size = 0;
+int scan_arr(int** arr_ptr) {
+    int capacity = 1;
+    int count = 0;
+    *arr_ptr = malloc(capacity * sizeof(int));
+
     int num;
     while (scanf("%d", &num) == 1) {
-        size++;
-        realloc(arr, size * sizeof(int));
-        arr[size - 1] = num;
+        if (count >= capacity) {
+            capacity *= 2;
+            *arr_ptr = realloc(*arr_ptr, capacity * sizeof(int));
+        }
+        (*arr_ptr)[count] = num;
+        count++;
+#ifdef DEBUG
+        printf("Filling arr: count %d, capacity %d\n", count, capacity);
+#endif
     }
 
-    return size;
+    if (count < capacity) {
+        *arr_ptr = realloc(*arr_ptr, count * sizeof(int));
+    }
+
+    return count;
 }
+
+#ifdef DEBUG
+void print_arr(int* arr, int size) {
+    for (int i = 0; i < size; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
+#endif
 
 int main(const int argc, char *argv[]) {
     if (argc == 1) {
@@ -43,14 +65,19 @@ int main(const int argc, char *argv[]) {
     }
 
     const int thread_count = atoi(argv[1]);
-    // Check that argv[0] is positive int
     if (thread_count <= 0) {
         fprintf(stderr, "Number of threads must be a positive integer\n");
         return 1;
     }
 
-    int* arr;
-    int size = scan_arr(arr);
+    int** arr_ptr = malloc(sizeof(int*));
+    int size = scan_arr(arr_ptr);
+    int* arr = *arr_ptr;
+
+#ifdef DEBUG
+    printf("Array size: %d\n", size);
+    print_arr(arr, size);
+#endif
 
     // TODO: gen thread data
 
@@ -58,7 +85,8 @@ int main(const int argc, char *argv[]) {
 
     // TODO: print
 
-    free(arr);
+    free(*arr_ptr);
+    free(arr_ptr);
 
     return 0;
 }
