f1d81e5097824b52c0cb15e205d3659f99f175b9
Nasonov Nikita <Cyclist2006@yandex.ru>
2026-03-20 14:19:04 +0300

    imp: comparators.c

--- task3/src/comparators.c
+++ task3/src/comparators.c
@@ -3,3 +3,27 @@
 //
 
 #include "comparators.h"
+
+#include <string.h>
+
+int compare_plain(const void *a, const void *b) {
+    const char *str1 = *(const char **)a;
+    const char *str2 = *(const char **)b;
+
+    return strcmp(str1, str2);
+}
+
+int compare_lex(const void *a, const void *b) {
+    const char *str1 = *(const char **)a;
+    const char *str2 = *(const char **)b;
+
+    return strcasecmp(a, b);
+}
+
+int compare_rplain(const void *a, const void *b) {
+    return compare_plain(b, a);  // Swap to get reverse order
+}
+
+int compare_rlex(const void *a, const void *b) {
+    return compare_lex(b, a);  // Swap to get reverse order
+}
