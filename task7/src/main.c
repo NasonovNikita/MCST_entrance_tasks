5b1b5450ac827aa3fe39c7dd4b92d5309a5171af
Nasonov Nikita <Cyclist2006@yandex.ru>
2026-03-24 17:01:10 +0300

    task7: prints

--- task7/src/main.c
+++ task7/src/main.c
@@ -4,6 +4,7 @@
 
 #include <signal.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
 
 #define PARENT_COPY_FILE_NAME "parent_copy"
@@ -11,6 +12,7 @@
 #define BUFFER_SIZE 1024
 
 void copy_file(FILE* original, FILE* copy) {
+    rewind(original);
     if (original == NULL || copy == NULL) {
         perror("fopen");
         return;
@@ -22,6 +24,16 @@ void copy_file(FILE* original, FILE* copy) {
     }
 }
 
+FILE* open_file(const char* file_name, const char* mode) {
+    FILE* file = fopen(file_name, mode);
+    if (file == NULL) {
+        perror("fopen");
+        exit(1);  // Terminal issue in this task, so exit
+    }
+
+    return file;
+}
+
 int main(const int argc, char* argv[]) {\
     if (argc != 2) {
         printf("Usage: %s <file_name>\n", argv[0]);
@@ -31,37 +43,30 @@ int main(const int argc, char* argv[]) {\
     const pid_t pid = fork();
 
     const char* file_name = argv[1];
-    FILE* input_file = fopen(file_name, "r");
-    if (input_file == NULL) {
-        perror("fopen");
-        return 1;
-    }
+    FILE* input_file = open_file(file_name, "r");
 
 
     if (pid == -1) {
         perror("fork");
         return 1;
-    } else if (pid == 0) {  // Child process
-        FILE* child_copy = fopen(CHILD_COPY_FILE_NAME, "w");
-        if (child_copy == NULL) {
-            perror("fopen");
-            return 1;
-        }
-
-        copy_file(input_file, child_copy);
-        fclose(child_copy);
-    } else {  // Parent process
-        FILE* parent_copy = fopen(PARENT_COPY_FILE_NAME, "w");
-        if (parent_copy == NULL) {
-            perror("fopen");
-            return 1;
-        }
-
-        copy_file(input_file, parent_copy);
-        fclose(parent_copy);
     }
 
+    FILE* output_file;
+
+    if (pid == 0) {  // Child process
+        output_file = open_file(CHILD_COPY_FILE_NAME, "w");
+        // Print Parent: [contents]
+        printf("Child: \n");
+    } else {  // Parent process
+        output_file = open_file(PARENT_COPY_FILE_NAME, "w");
+        printf("Parent: \n");
+    }
+    copy_file(input_file, output_file);
+    copy_file(input_file, stdout);
+    printf("\n");  // Add newline after printing contents
+
     fclose(input_file);
+    fclose(output_file);
 
     return 0;
 }
