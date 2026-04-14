3f95c57bc001e0ed89eaf3fcdceaa7825118bee3
Nasonov Nikita <Cyclist2006@yandex.ru>
2026-04-14 14:20:17 +0300

    task10: full parsing and error handling

--- task10/include/parser.h
+++ task10/include/parser.h
@@ -8,13 +8,18 @@
 
 #include <cstddef>
 #include <string>
-#include <map>
+#include <unordered_map>
 #include <functional>
 
 constexpr size_t VARIABLES_COUNT = 26;
 
-using evaluate_t = std::function<bool(const std::map<char, bool>&)>;
+using evaluate_t = std::function<bool(const std::unordered_map<char, bool>&)>;
 
-evaluate_t parse(const std::string &expression);
+typedef struct {
+    evaluate_t evaluate;
+    std::vector<char> variables;
+} expression_t;
+
+expression_t parse(const std::string &expression);
 
 #endif //PARSER_H
--- task10/src/parser.cpp
+++ task10/src/parser.cpp
@@ -3,34 +3,87 @@
 //
 
 #include "parser.h"
+
+#include <stdexcept>
 #ifdef DEBUG
 #include <iostream>
 #endif
 
-constexpr char OR = '|';
-constexpr char AND = '&';
-constexpr char NOT = '!';
-constexpr char OPEN_BRACKET = '(';
-constexpr char CLOSE_BRACKET = ')';
+const std::string OR = "OR";
+const std::string AND = "AND";
+const std::string NOT = "NOT";
+const std::string OPEN_BRACKET = "(";
+const std::string CLOSE_BRACKET = ")";
 
 class Parser {
 public:
     explicit Parser(std::string to_parse) : expr(std::move(to_parse)) {}
 
-    evaluate_t parse() { return parseExpr(); }
+    void parse() {
+        pos = 0;
+#ifdef DEBUG
+        std::cout << "Parsing: " << expr << std::endl;
+#endif
+        eval_func = parseExpr();
+
+        if (pos != expr.size())
+            throw std::runtime_error("Error while parsing expression");
+    }
+
+    [[nodiscard]] evaluate_t get_eval_func() const { return eval_func; }
+    [[nodiscard]] std::vector<char> get_variables() const { return variables; }
 private:
+    evaluate_t eval_func;
+    std::vector<char> variables;
     std::string expr;
+    bool latest_is_variable = false;
     size_t pos = 0;
 
-    char peek() {
-        while (pos < expr.size() && isspace(expr[pos])) pos++;
-        return pos < expr.size() ? expr[pos] : 0;
+    std::string peek() {
+        const size_t preserve_pos = pos;
+
+        std::string elem = get();
+        pos = preserve_pos;
+#ifdef DEBUG
+        if (!elem.empty())
+            std::cout << "Peeked: " << elem << std::endl;
+#endif
+        return elem;
     }
-    char get() { return expr.at(pos++); }
+
+    std::string get() {
+        // Skip spaces
+        while (pos < expr.size() && isspace(expr[pos])) pos++;
+
+        if (pos >= expr.size()) {
+#ifdef DEBUG
+            std::cout << "End of expr" << std::endl;
+#endif
+            return "";
+        }
+
+        const size_t start = pos;  // Start of elem
+
+        // Find the end of the current elem
+        while (pos < expr.size() && !isspace(expr[pos])) {
+            pos++;
+
+            // Return brackets immediately
+            if (expr[pos - 1] == OPEN_BRACKET.at(0) ||
+                expr[pos - 1] == CLOSE_BRACKET.at(0)) {
+                break;
+            }
+        }
+
+        std::string res = expr.substr(start, pos - start);
+
+        return res;
+    }
 
     evaluate_t parseExpr() {
         auto left = parseAnd();
         while (peek() == OR) {
+            latest_is_variable = false;
             get();
             auto right = parseAnd();
 
@@ -51,6 +104,7 @@ private:
     evaluate_t parseAnd() {
         auto left = parseFactor();
         while (peek() == AND) {
+            latest_is_variable = false;
             get();
 
             auto right = parseFactor();
@@ -66,26 +120,43 @@ private:
     }
 
     evaluate_t parseFactor() {
-        char c = peek();
-        if (c == NOT) {
+        std::string elem = peek();
+        if (elem == NOT) {
+            latest_is_variable = false;
             get();
             auto inner = parseFactor();
-            return [f = std::move(inner)](const auto& v) { return !f(v); };
+            return [f = std::move(inner)](const auto& v) {
+
+#ifdef DEBUG
+                std::cout << " NOT "  << f(v) << std::endl;
+#endif
+                return !f(v);
+            };
         }
-        if (c == OPEN_BRACKET) {
+        if (elem == OPEN_BRACKET) {
             get();
             auto inner = parseExpr();
             get(); // Closing bracket
             return inner;
         }
 
-        c = get();
-        return [c](const auto& v) { return v.at(c); };
+        if (latest_is_variable) {
+            throw std::runtime_error("Two variables in a row");
+        }
+
+        elem = get();
+        variables.push_back(elem.at(0));
+        latest_is_variable = true;
+        return [elem](const auto& v) { return v.at(elem.at(0)); };
     }
 };
 
-evaluate_t parse(const std::string &expression) {
+expression_t parse(const std::string &expression) {
     Parser parser(expression);
+    parser.parse();
 
-    return parser.parse();
+    expression_t result;
+    result.evaluate = parser.get_eval_func();
+    result.variables = parser.get_variables();
+    return result;
 }
